0ba1619b356db1c0d3109733b73a4fcc7988c8a1
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-04-14 16:53:20 +0300

    5: +application properties Creat Drop and other properties added +Models updated

--- src/main/java/lt/codeacademy/blogproject/model/Comment.java
+++ src/main/java/lt/codeacademy/blogproject/model/Comment.java
@@ -1,12 +1,20 @@
 package lt.codeacademy.blogproject.model;
 
 import lombok.Data;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
+@Entity
+@Table(name = "Comments")
 public class Comment {
 
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "VARCHAR(36)", updatable = false)
+    @Type(type = "uuid-char")
     private UUID commentID;
     private String description;
     private UUID userID;
--- src/main/java/lt/codeacademy/blogproject/model/User.java
+++ src/main/java/lt/codeacademy/blogproject/model/User.java
@@ -1,12 +1,20 @@
 package lt.codeacademy.blogproject.model;
 
 import lombok.Data;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
+@Entity
+@Table(name = "Users")
 public class User {
 
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "VARCHAR(36)" , updatable = false)
+    @Type(type = "uuid-char")
     private UUID userID;
     private String username;
     private String password;
