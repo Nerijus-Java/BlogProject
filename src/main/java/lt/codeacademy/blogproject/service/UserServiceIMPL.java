5c372ed5318e7eeeea3461e49bda8565e88e65c4
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-05-10 17:19:35 +0300

    clean up

--- src/main/java/lt/codeacademy/blogproject/service/UserServiceIMPL.java
+++ src/main/java/lt/codeacademy/blogproject/service/UserServiceIMPL.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-@Primary
 public class UserServiceIMPL implements UserDetailsService {
     private final UserRepository userRepository;
 
